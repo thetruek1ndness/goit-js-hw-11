fbf376fcba6f45e40e6afcc0236938cee0ea1fe4
thetruek1ndness <andrii.ptrnk@gmail.com>
2023-05-08 12:00:29 +0300

    fix

--- src/js/api-set.js
+++ src/js/api-set.js
@@ -11,7 +11,7 @@ export default class NewsApiService {
       method: 'get',
       url: 'https://pixabay.com/api/',
       params: {
-        key: '34523545-f21683fd59bfc3e4e2549fe07',
+        key: '36158695-865d46ee774963d2fc0d5425f',
         q: `${this.searchQuery}`,
         image_type: 'photo',
         orientation: 'horizontal',
